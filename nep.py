c708e99d4df157cefa2aaca7fc734e25e84a521f
suraj dhakal <surajdhakal427@gmail.comgit config --global user.name surajdhakal427@gmail.comgit config --global user.email surajdhakal427@gmail.com>
2018-01-14 15:13:23 +0545

    feature game over

--- nep.py
+++ nep.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 pygame.init()
 red=(255,0,0)
 black=(255,255,255)
@@ -14,6 +15,10 @@ lead_x_change=0
 lead_y_change=0
 block_size=10
 clock=pygame.time.Clock()
+font =pygame.font.SysFont(None,25)
+def message_to_screen(msg,color):
+        Screen_text=font.render(msg,True,color)
+        screen.blit(Screen_text,[display_width/2, display_height/2])
 while not gameExit:
         for event in pygame.event.get():
                 if event.type ==pygame.QUIT:
@@ -33,15 +38,16 @@ while not gameExit:
                                 lead_x_change=0
         if lead_x>=display_width or lead_x<=0 or lead_y>=display_height or lead_y<=0:
                 gameExit = True
-
         lead_x+=lead_x_change
         lead_y+=lead_y_change
 
                 #print(event)
         screen.fill(red)
-        pygame.draw.rect(screen,black,[lead_x,lead_y,15,15])
+        pygame.draw.rect(screen,black,[lead_x,lead_y,block_size,block_size])
         pygame.display.update()
         clock.tick(fps)
-
+message_to_screen("sorry game end here ,my bad",black)
+pygame.display.update()
+time.sleep(2)
 pygame.quit()
 quit()
